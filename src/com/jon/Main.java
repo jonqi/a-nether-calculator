9f9ceec4650c87670ae36847d73f742de231ea67
jon <jonathanjiaqianqi@gmail.com>
2020-06-20 14:16:22 -0700

    Add java project

---
+++ src/com/jon/Main.java
@@ -0,0 +1,23 @@
+package com.jon;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        while(true) {
+            print("\nInput scaanner coordinate.");
+            Scanner scaanner = new Scanner(System.in);
+            if (scaanner.hasNextDouble()) {
+                print("scaanner coordinate: " + scaanner.nextDouble());
+            } else {
+                print("Please enter a number.");
+            }
+        }
+    }
+
+    private static void print(String s) {
+        System.out.println(s);
+    }
+}
